57ef9ce9cc25e4287e1bcc52bf9abd373031647b
Andrey Litvinov <litwinoff@gmail.com>
2016-12-13 19:54:47 +0300

    Own annotations

--- src/main/java/annotations/annotations/Service.java
+++ src/main/java/annotations/annotations/Service.java
@@ -1,7 +1,16 @@
 package annotations.annotations;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-
+@Documented 			  			// class marked by @Service will go to JavaDoc
+@Inherited  			  			// all children will also be marked by @Service
+@Target(ElementType.TYPE) 			// scope of usage is on classes, IFs and ENUMs
+@Retention(RetentionPolicy.RUNTIME) // time of life: always
 public @interface Service {
 
 	String name(); // mandatory property of annot @Service(name="Blah")
