a541c4852c1d0ce8a41d8e9def1978642b51b189
Andrey Litvinov <litwinoff@gmail.com>
2016-12-14 16:12:32 +0300

    Loading services dynamically by name of @Service

--- src/main/java/annotations/annotations/AnnotationProcessor.java
+++ src/main/java/annotations/annotations/AnnotationProcessor.java
@@ -1,15 +1,63 @@
 package annotations.annotations;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AnnotationProcessor {
+	
+	// <name of annotation of the service, object of service>
+	private static Map<String, Object> servicesMap = new HashMap<>();
 
 	public static void main(String[] args) {
 		inspectService(SimpleService.class);
 		inspectService(LazyService.class);
 		inspectService(String.class);
+		
+		// creating services dynamically by the name of the class
+		loadService("annotations.annotations.SimpleService");
+		loadService("annotations.annotations.LazyService");
+		loadService("java.lang.String");
+	
+		checkObject(servicesMap.get("weaponsSystem"));
+		checkObject(servicesMap.get("heatingSystem"));
+		checkObject(servicesMap.get("coolingSystem"));
+		
 	}
 	
+	private static void checkObject(Object serviceObj) {
+		if (serviceObj == null) {
+			System.out.println("Object NOT found");
+		} else {
+			System.out.println("Object " + serviceObj.getClass().getSimpleName() + " found");
+		}		
+	}
+
+	private static void loadService(String className) {
+		try {
+			// reflection
+			Class<?> clazz = Class.forName(className);
+						
+			// if class of the service has annotation @Service
+			if(clazz.isAnnotationPresent(Service.class)) {
+			
+				Service ann = clazz.getAnnotation(Service.class);
+				
+				// creating new object of service class
+				Object serviceObj = clazz.newInstance();
+				
+				// add to map by name from annotation <name,Object>						
+				servicesMap.put(ann.name(), serviceObj);		
+			}
+			
+		} catch (ClassNotFoundException  | InstantiationException | IllegalAccessException e) {
+			// Java 7 feature
+			e.printStackTrace();
+		}
+		
+		
+	}
+
 	static void inspectService(Class<?> service) {
 		if(service.isAnnotationPresent(Service.class)) {
 			Service ann = service.getAnnotation(Service.class);
@@ -23,9 +71,7 @@ public class AnnotationProcessor {
 					System.out.println("Method " + method.getName() + 
 							" is NOT annotated");
 				}
-			}
-			
-			
+			}			
 			
 		} else {
 			System.out.println("Sorry! Annotation '"
@@ -36,5 +82,6 @@ public class AnnotationProcessor {
 		System.out.println("\n");
 		
 	}
+
 
 }
